207bbb6d9ad08a14b10869f288d7c1a1406a3ae9
Sergey Plotnikov <sergey.plotnikov@samgau.com>
2019-10-28 18:10:32 +0600

    First page

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+const CategoryMealsScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meal Screen</Text>
+      <Button title='Go to Details'
+        onPress={() => {
+          props.navigation.navigate('MealDetail');
+        }} />
+      <Button title='Go Back'
+        onPress={() => {
+          props.navigation.goBack();
+        }} />
+    </View >
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default CategoryMealsScreen
